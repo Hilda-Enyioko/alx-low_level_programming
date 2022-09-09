dddb97b76b20614589620c5edbf8595f5ee24557
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-09 22:11:59 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * Description: Print all single digits of base 10
- * return: Always 0
- */
+* main - Entry point
+* Description: Print all single digits of base 10
+* return: Always 0
+*/
 int main(void)
 {
-  int a;
-  for (a = 0; a < 10; a++)
-    {
-      printf("%d", a);
-    }
-  printf("\n");
-  return (0);
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
 }
