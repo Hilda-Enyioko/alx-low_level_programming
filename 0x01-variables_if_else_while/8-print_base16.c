8cce70b10ea8e021aba5a8933c63cd286cd91f7e
Hilda Enyioko <enyiokohilda@gmail.com>
2022-09-09 15:30:49 -0700

    hexadecimal digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: prints all the numbers of base 16 in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+  int i;
+  
+  for (i = 48; i < 58; i++) 
+    {
+     putchar(i);
+    }
+  for (i = 'a'; i <= 'f'; i++)
+    {
+      putchar(i);
+    }
+  putchar('\n'); 
+  return (0); 
+}
