4fb657cbc9ef048c98e3bc265f0a96f53af833bf
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-09 23:36:31 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 int i;
-  
-for (i = 48; i < 58; i++) 
+
+for (i = 48; i < 58; i++)
 {
 putchar(i);
 }
@@ -18,6 +18,6 @@ for (i = 'a'; i <= 'f'; i++)
 {
 putchar(i);
 }
-putchar('\n'); 
-return (0); 
+putchar('\n');
+return (0);
 }
