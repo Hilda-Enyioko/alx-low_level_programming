a8a3ffa90f09155285c150bde8d8617a6a2bff3f
Hilda Enyioko <enyiokohilda@gmail.com>
2022-09-09 15:12:07 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: Print alphabet backwards
+ * Return: Always 0
+ */
+int main(void)
+{
+  char a;
+  for (a = 'z'; a <= 'z'; a--)
+    {
+      putchar(a);
+    }
+  putchar('\n');
+  return (0);
+}
