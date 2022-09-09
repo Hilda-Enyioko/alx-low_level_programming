ff5375f0e25e824db687f20b2ad58aa4ed9dba68
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-09 22:38:00 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * Description: Print numbers of single digit in base 10
- * Return: Always 0
- */
+* main - Entry point
+* Description: Print numbers of single digit in base 10
+* Return: Always 0
+*/
 int main(void)
 {
-  int a;
-  for (a = 48; a < 58; a++)
-    {
-      putchar(a);
-    }
-  putchar('\n');
-  return (0);
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
