5a0d3b10547c66bc416189e8d92b8d664c9575cb
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-13 21:59:58 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,21 +7,21 @@
  */
 void times_table(void)
 {
-  int a;
-  int b;
-  int c;
+int a;
+int b;
+int c;
 
-  for (a = 0; a <= 9; a++)
-    {
-      for (b = 0; b <= 9; b++)
-	{
-	  c = a * b;
-	  _putchar(c + '0');
-	  _putchar(',');
-	  _putchar(' ');
-	}
-      _putchar('\n');
-    }
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+_putchar(c + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
 }
 
        
