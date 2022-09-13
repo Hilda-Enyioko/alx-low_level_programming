4bac74010021b581be0883a5ef6dbe4c3d48dd23
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-13 22:53:04 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <studio.h>
+
+/**
+* add - to sum two values
+* @a: value to be added
+* @b: value to be added
+* Return: Always 0
+*/
+int add(int a, int b)
+{
+int c = a + b;
+return (c);
+}
