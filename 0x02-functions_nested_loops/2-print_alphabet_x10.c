ee71585c301b4840699e153902b8aa858ff479f6
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-13 15:56:19 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
  */
 void print_alphabet_x10(void)
 {
-  int b;
-  for (b = 1; b <= 10; b++)
-    {
-      char a;
-	for (a = 'a'; a <= 'z'; a++)
-	  {
-	    _putchar(a);
-	  }
-      _putchar('\n');
-	}
+int b;
+for (b = 1; b <= 10; b++)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
 }
