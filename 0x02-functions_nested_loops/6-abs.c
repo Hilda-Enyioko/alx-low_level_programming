77ed54a32617a61bee868565cdcb6272fb9e9be8
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-13 19:21:51 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,18 +3,18 @@
 
 /**
  * _abs: prints absolute value
- *
+ * n: number that is tested
  * Return: Always 0
  */
 int _abs(int n)
 {
-  if (n < 0)
-    {
-      return (n * -1);
-    }
-  else
-    {
-      return (n);
-    }
-  return (0);
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
+return (0);
 }
