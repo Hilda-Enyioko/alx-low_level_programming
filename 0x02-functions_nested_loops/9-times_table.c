86da460b4350e3831b420ae07d1c078eced1ae17
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-13 22:23:11 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,7 +25,7 @@ _putchar('0');
 if (b != 0)
 {
 _putchar(' ');
-_putchar((c % 10) + '0');
+_putchar((c % 10) + '0');
 }
 if (b < 9)
 {
@@ -35,9 +35,9 @@ _putchar(' ');
 }
 else
 {
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
-if (b < 9)
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+if (b < 9)
 {
 _putchar(c + '0');
 _putchar(',');
