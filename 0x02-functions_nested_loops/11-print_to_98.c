e689db05bda9436eda7d055b5b42690be06033a2
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-14 16:32:36 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (;n <= 98; n++)
+for (; n <= 98; n++)
 {
 printf("%d", n);
 printf(", ");
@@ -23,9 +23,10 @@ continue;
 }
 printf("\n");
 }
+
 if (n >= 98)
 {
-for (;n >= 98; n--)
+for (; n >= 98; n--)
 {
 printf("%d", n);
 printf(", ");
