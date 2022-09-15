98a42bd7fe481ee5b07b54eca622b31517408aaa
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-15 20:50:15 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 int a = 1;
 int b;
 
-while (a < size && size > 0)
+while (a <= size && size > 0)
 {
 b = 0;
 while (b < size - a)
@@ -29,6 +29,6 @@ b++;
 _putchar('\n');
 a++;
 }
-if (a <= 1)
+if (a == 1)
 _putchar('\n');
 }
