25fb9bd9a9c5b0c5b3d413cbad31c91ce3233a4c
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-19 13:27:05 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 
 /**
  * reset_to_98 - function that takes a pointer to an int, updates it to 98
- * @*n: pointer to the address of the var int
+ * @n: pointer to the address of the var int
  * Return: Always 0
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
