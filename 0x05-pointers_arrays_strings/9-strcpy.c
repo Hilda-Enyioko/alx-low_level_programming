3640184cce7b141a0652781081454d2f4ba848ed
Hilda Enyioko <enyiokohilda@gmail.com>
2022-09-19 13:39:41 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,13 +4,13 @@
 * *_strcpy - copies a string from one pointer to another
 * @dest: pointer receiving the copied string
 * @src: pointer with the string to be copied
-* Description: a function that copies a string including the terminating null byte
+* Description: a function that copies a string including the terminating byte
 * Return: 0
 */
 char *_strcpy(char *dest, char *src)
 {
 int i;
-for (i = 0; src[i]; i++)
+for (i = -1; src[i] != 0; i++)
 {
 dest[i] = src[i];
 }
