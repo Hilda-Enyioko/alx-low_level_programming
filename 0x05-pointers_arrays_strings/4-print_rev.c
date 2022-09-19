556a268ffc3afaa6d3a06865a5b1320688d71e5a
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-19 15:47:43 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,14 @@
  */
 void print_rev(char *s)
 {
-  int i;
-  i = 0;
-  while (s[i])
-    {
-    i++;
-    while (i--)
-      {
-	_putchar(s[i]);
-	i--;
-      }
-    }
-    _putchar('\n');
+int i;
+i = 0;
+while (s[i])
+i++;
+
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
