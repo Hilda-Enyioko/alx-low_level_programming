8ea840aa8125f27fb7953e0735f2ef797c375c60
Hilda Enyioko <enyiokohilda@gmail.com>
2022-09-19 06:42:00 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void _puts - function that prints a string, followed by a new line
+ * @str: string to be printed
+ * Description: a function that prints a string, followed by a new line
+ * Return: 0
+ */
+void _puts(char *str)
+{
+  int i;
+  for (i = 0; str[i]; i++)
+    {
+      _putchar(str[i]);
+    }
+  _putchar('\n');
+}
+
+     
