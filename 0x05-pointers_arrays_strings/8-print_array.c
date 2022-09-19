71e685e4665c5d1588e245e3d4e5532474e25fd1
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-19 21:01:42 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 
 /**
-* print_array - prints n elements of an array of integers followed by a new line
+* print_array - prints n elements of an array of integers followed by new line
 * @a: array to be printed
 * @n: number of elemenrs in the array to be printed
 * Description: a function that prints n elements of an array
 * of integers followed by a new line
 * Numbers must be separated by comma, followed by a space
-* The numbers should be displayed in the same order as they are stored in the array
+* The numbers should be displayed in the order they are stored in the array
 * Return: Nothing
 */
 void print_array(int *a, int n)
 {
 int i = 0;
-while (i <= n)
+while (i < n)
 {
 printf("%d", a[i]);
-if (i <= n - 1)
+if (i < n - 1)
 {
 printf(", ");
 }
