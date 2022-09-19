70be07e102bb99ff0a62eb88be8578d1a6687f7b
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-19 18:28:45 +0100

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* puts2 - function that prints consecutive odd characters in a string
+* @str: string to be tested
+* Return: Nothing
+*/
+void puts2(char *str)
+{
+int i;
+for (i = 0; s[i]; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(s[i]);
+}
+}
+_putchar('\n');
+}
