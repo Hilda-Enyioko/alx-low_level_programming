f7a7af2327e5d1248c6b06d05a69fc5d212d896e
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-19 17:47:04 +0100

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rev_string - function that reverses a string
+* @s: string to be reversed
+* Description: a function that reverses a string
+* Return: 0
+*/
+void rev_string(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
