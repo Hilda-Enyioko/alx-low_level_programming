b111b0b87ff8251cdd9a913c73b936884ef7b7e7
Hilda Enyioko <enyiokohilda@gmail.com>
2022-09-21 09:10:10 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strcncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - function that copies a string
+ * @dest: destination pointer receiving string
+ * @src: source pointer for the string
+ * @n: number of times to copy
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+  int i = 0;
+  do {
+    i++;
+    dest[i] = src[i];
+  } while (src[i] != '\0' && i < n);
+  return (dest);
+}
