cf1188a52730d75dc085189c756f8ee57b4e865a
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-22 13:13:18 +0100

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* leet - function that encodes a string into 1337
+* @str: string to be tested
+* Return: 0
+*/
+char *leet(char *str)
+{
+int i;
+int j;
+int a[];
+int b[];
+int c[];
+a[] = "AEOTL";
+b[] = "aeotl";
+c[] = "43071";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 5; j++)
+{
+if (s[i] == a[j] || s[i] == b[j])
+{
+s[i] = c[j];
+}
+}
+}
+return (s);
+}
