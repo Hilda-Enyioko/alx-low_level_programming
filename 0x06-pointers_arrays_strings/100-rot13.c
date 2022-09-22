459dc638f3a2012fa9d6707a62ef1f2f25507694
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-22 18:33:19 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,7 +16,7 @@ for (i = 0; str[i] != '\0'; i++)
 {
 for (j = 0; j < 52; j++)
 {
-if (((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) && (str[i] == a[j]))
+if (str[i] == a[j])
 {
 str[i] = b[j];
 }
