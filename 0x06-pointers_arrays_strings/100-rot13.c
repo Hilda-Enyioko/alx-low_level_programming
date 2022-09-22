5847c5dacaa152a2123b545110f7d0e0162b6969
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-22 18:12:13 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,5 +6,21 @@
 * @str: string to be tested
 * Return: Encode str
 */
-char *rot13(char *)
+char *rot13(char *str)
 {
+int i;
+int j;
+int a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+int b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < 54; j++)
+{
+if (str[i] == a[j])
+{
+str[i] = b[j];
+}
+}
+}
+return (str);
+}
