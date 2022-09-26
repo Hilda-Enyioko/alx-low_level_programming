9a5a6bc2bd56ea42065d6fbb53638909f3bd8a71
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-26 22:27:04 +0100

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches a string for any of a set of bytes
+* @s: string to be checked
+* @accept: string to be matched
+* Return: NULL or pointer to the byte
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+char *p;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0', j++)
+{
+if (s[i] == accept[j])
+{
+p = &s[i];
+}
+return (p);
+}
+return (0);
+}
