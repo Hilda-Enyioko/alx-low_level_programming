de07eabc92f83c87f78d95ce7bfd5513d38826d3
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-26 22:36:37 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,11 +14,11 @@ for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
 {
-if (accept[j] == s[i])
+if (s[i] == accept[j])
 {
 p = &s[i];
 }
-return (p);
+return (*p);
 }
 }
 return (0);
