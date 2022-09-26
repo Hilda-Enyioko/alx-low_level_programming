25de8fe946dc33e55828a4c7bb2a04e1cd5992be
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-26 23:09:01 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,12 +11,18 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i, j;
-for (i = 0; haystack[i] != '\0'; i++)
+for (i = 0; haystack[i] > '\0'; i++)
 {
-for (j = 0; needle[j] != '\0'; j++)
+for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
 {
-if (haystack[i] == needle[j])
-return (s + i);
+if (haystack[j] != needle[j - i])
+{
+break;
+}
+}
+if (needle[j - i] == '\0')
+{
+return (haystack + i);
 }
 }
 return (0);
