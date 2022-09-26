12be502af0d1f1eb8b30713929f00329927aba01
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-26 13:31:46 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
 * @n: maximum bytes to copy
 * Return: dest
 */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 for (i = 0; n > 0; i++, n--)
