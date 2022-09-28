fa537c01be491db7493d9c37fc746205c8aa678a
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-28 14:40:35 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,10 @@
  */
 int _strlen_recursion(char *s)
 {
-  int n = 0;
-  if (*s > '\0')
-    {
-      n += _strlen_recursion(s + 1) + 1;
-    }
-  return (n);
+int n = 0;
+if (*s > '\0')
+{
+n += _strlen_recursion(s + 1) + 1;
+}
+return (n);
 }
