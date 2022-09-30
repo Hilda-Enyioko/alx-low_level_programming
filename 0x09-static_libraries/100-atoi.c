432b47183a29b541e41f899a83a6d5829e639499
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-30 22:47:53 +0100

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -7,20 +7,20 @@
  */
 int _atoi(char *s)
 {
-  int a = 1, b = 0, c;
-  for (c = 0; s[c] >= 48 && s[c] <= 57; c++)
-    {
-      if (s[c] == '-')
-	{
-	  a *= -1;
-	}
-    }
-  for (int i = c; s[i] >= 48 && s[i] <= 57; i++)
-    {
-      b *= 10;
-      b += (s[i] - 48);
-    }
-  return (a * b);
+int a = 1, b = 0, c;
+for (c = 0; s[c] >= 48 && s[c] <= 57; c++)
+{
+if (s[c] == '-')
+{
+a *= -1;
+}
+}
+for (int i = c; s[i] >= 48 && s[i] <= 57; i++)
+{
+b *= 10;
+b += (s[i] - 48);
+}
+return (a * b);
 }
 
  
