c7d0c9d7fae3ffef3d4eedc5db569aef7f248bc3
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-30 22:42:16 +0100

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -9,16 +9,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int a;
-  int i;
-  for (i = 0; dest[i] != '\0'; i++)
-    {
-      i++;
-    }
-  for (a = 0; src[a] != '\0'; a++)
-    {
-      dest[i] = src[a];
-      i++;
-    }
-  return (dest);
+int a;
+int i;
+for (i = 0; dest[i] != '\0'; i++)
+{
+i++;
+}
+for (a = 0; src[a] != '\0'; a++)
+{
+dest[i] = src[a];
+i++;
+}
+return (dest);
 }
