1f18da624458edd5866eb1b29eece9574de00fde
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-30 18:01:26 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,2 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+
+
+/**
+* main - prints program name, followed by a new line
+* @argc: argument count(int)
+* @argv: argument vector(array)
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
