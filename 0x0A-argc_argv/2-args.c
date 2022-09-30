43d41c111efd00c3a7dfe063d3a4dcaed07d0aae
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-30 18:21:39 +0100

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - prints all arguments a program receives
+* @argc: argument count(int)
+* @argv: argument vector(array)
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i= 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
