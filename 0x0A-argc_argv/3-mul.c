8cf925601262b6e9db08645c7b8e47bbc77a27c1
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-30 19:39:53 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,11 +12,11 @@ int main(int argc, char const *argv[])
 {
 int a = atoi(argv[1]) * atoi(argv[2]);
 (void)argc;
-if (argc != 3)
+if (argc == 3)
 {
-printf("Error\n");
-return (1);
-}
 printf("%i\n", a);
 return (0);
+}
+printf("Error\n");
+return (1);
 }
