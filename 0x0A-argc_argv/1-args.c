804ba12170f57f9bb6a5109a5254354e5228f351
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-30 18:08:21 +0100

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - prints the number of arguments passed into it
+* @argc: argument count(int)
+* @argv: argument vector(array)
+* Return: 0
+*/
+int main(int argc, int *argv[])
+{
+(void)argv;
+printf("%d", argc - 1);
+return (0);
+}
