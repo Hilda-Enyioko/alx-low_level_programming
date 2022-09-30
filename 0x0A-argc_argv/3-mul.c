fa533fa1ed9a12d2d1e4fdc63cbb9c952756f430
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-09-30 18:40:30 +0100

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - multiplies two numbers
+* @argc: argument count(int)
+* @argv: argument vector(array)
+* Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+(void)argc;
+int a = atoi(argv[1]) * atoi(argv[2]);
+if (argc != 3)
+{
+printf("ERROR\n");
+return (1);
+}
+printf("%d\n", a);
+return (0);
+}
