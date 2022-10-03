c9ca7bd5fed3bf77a00a92a988d50e21357b7fd2
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-03 22:23:37 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * _strlen - a function that prints the length of a string
+ * @s: char pinter that oiints to the char string
+ * Description: a function that returns the length of a string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+return (i);
+}
+
+
+/**
+* _strcat - function that concatenates two strings
+* @dest: string that is appended to src strong
+* @src: string that is appended to dest strong
+* Return: Always 0
+*/
+char *_strcat(char *dest, char *src)
+{
+int a;
+int i;
+for (i = 0; dest[i] != '\0'; i++)
+{
+i++;
+}
+for (a = 0; src[a] != '\0'; a++)
+{
+dest[i] = src[a];
+i++;
+}
+return (dest);
+}
+
+
 /**
 * *str_concat - concatenates two strings
 * @s1: first string
@@ -11,30 +50,28 @@
 char *str_concat(char *s1, char *s2)
 {
 char *p;
-int i, size1, size2;
-int mid;
+int size;
+int size1 = _strlen(s1);
+int size2 = _strlen(s2);
 if (s1 == NULL)
+{
 s1 = "";
+}
 if (s2 == NULL)
+{
 s2 = "";
-for (size1 = 0; s1[size1] != '\0'; size1++)
-;
-for (size2 = 0; s2[size2] != '\0'; size2++)
-;
-p = malloc(sizeof(char) * (size1 + size2 + 1));
+}
+p = malloc(sizeof(char) * (size1 + size2) + 1);
 if (p == NULL)
 {
-free(p);
 return (NULL);
 }
-for (i = 0; i < 1; i++)
+size = 0;
+while (size <= size1 + size2)
 {
-p[i] = s1[i];
+p[size] = _strcat(s1, s2);
 }
-mid = size2;
-for (size2 = 0; size2 <= mid; size2++, i++)
-{
-p[i] = s2[i];
+p[size] = '\0';
+return (m);
 }
-return (p);
-}
+
