91483e2ef0cd5a84e66805f7ceb085afbb88ce6b
Hilda Enyioko <enyiokohilda@gmail.com>
2022-10-04 02:01:06 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - duplicates a string
+ * @str: string to be duplicated
+ * Return: pointer to new string or NULL
+ */
+char *_strdup(char *str)
+{
+  int i;
+  int size;
+  char *ptr = malloc(size
+		     
