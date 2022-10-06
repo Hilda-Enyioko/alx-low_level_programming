75e6b18adcec48d728120289fe37a39288b8c8d9
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-06 16:03:13 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ return (NULL);
 ptr = malloc((max - min + 1) * sizeof(int));
 if (ptr == 0)
 return (0);
-for (a = 0; ptr[a] != '\0' && min + a <= max; a++)
+for (a = 0; min + a <= max; a++)
 ptr[a] = min + a;
 return (ptr);
 }
