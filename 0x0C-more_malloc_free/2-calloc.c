41dada04bfa0727c0a907917454833eaefb525c7
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-06 17:54:24 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,8 +7,7 @@
  * @n: maximum bytes to be filled
  * Return: s
  */
-
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 char *ptr = s;
 while (n--)
@@ -23,7 +22,7 @@ return (ptr);
  * @size: size of array
  * Return: pointer or NULL
  */
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
 if (nmemb == 0 || size == 0)
@@ -33,4 +32,4 @@ if (ptr == 0)
 return (NULL);
 _memset(ptr, 0, nmemb * size);
 return (ptr);
-}{
+}
