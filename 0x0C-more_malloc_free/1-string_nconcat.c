0109f568e318191d3e18c27afda5966fa366bb7d
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-06 15:25:22 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,7 @@
  * Description: a function that returns the length of a string
  * Return: 0
  */
-int _strlen(char *s)
+unsigned int _strlen(char *s)
 {
 int i = 0;
 while (s[i] != '\0')
@@ -26,20 +26,20 @@ return (i);
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int a, b;
-int i, j;
+unsigned int a, b;
+unsigned int i, j;
 char *ptr;
 if (s1 == NULL)
-s1 "\0";
+s1 = "\0";
 if (s2 == NULL)
-s2 "\0";
+s2 = "\0";
 a = _strlen(s1);
 b = _strlen(s2);
-if (n < _strlen(s2))
+if (n < b)
 ptr = malloc(a + n * sizeof(char) + 1);
 else
 ptr = malloc(a + b + 1);
-if ptr == 0
+if (ptr == 0)
 return (0);
 for (i = 0; s1[i] != '\0'; i++)
 ptr[i] = s1[i];
