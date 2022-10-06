fe13ab5394e7b6d686fb89bfacefc5a584ab8071
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-06 18:28:11 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,4 +5,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 unsigned int _strlen(char *s);
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n);
