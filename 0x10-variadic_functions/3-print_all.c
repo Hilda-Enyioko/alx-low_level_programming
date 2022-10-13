80de03bcbda72691d503735ad4e917c4faee093d
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-13 18:49:21 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,7 @@
 
 /**
  * print_all - prints anything
- * @format: a list of types of arguments passed to the function
+ * @format: list of types of arguments passed to function
  * Return: Nothing
  */
 void print_all(const char * const format, ...)
