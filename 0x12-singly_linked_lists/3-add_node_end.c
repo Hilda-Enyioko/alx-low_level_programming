426d3e77be559cc85e55047c8d71f0c7c99b8f62
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-20 19:12:34 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new;
-list_t *last
+list_t *last;
 int i; /*length of str*/
 char strDup; /*duplicate of str*/
 new = malloc(sizeof(list_t));
