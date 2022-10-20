307fba01122e520b66d271f295d4dfd989171f03
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-20 17:38:48 +0100

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: single linked list
+ * Return: number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
