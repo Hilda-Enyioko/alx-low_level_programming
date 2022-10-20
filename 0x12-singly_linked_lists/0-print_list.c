087a3976767d1b35e39da7a979a2ae89ccd6bd2b
Hilda Enyioko <enyiokohilda@gmail.com>
2022-10-20 07:47:15 -0700

    project 12

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_list - prints all elements of a list_t list.
+ * h: singley linked list
+ * Return: number of node present in list h
+ */
