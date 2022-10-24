1cd0f5ac456c10db1e122a9b157d3cb8be14ba61
Hilda Enyioko <enyiokohilda@gmail.com>
2022-10-24 07:12:47 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: singly linked list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+  size_t nnodes;
+  nnodes = 0;
+  while (h != NULL)
+    {
+      h = h->next;
+      nnodes++;
+    }
+  return (nnodes);
+}
