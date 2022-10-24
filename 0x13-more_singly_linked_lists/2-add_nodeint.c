09fad0fac343d29f869a387790caba88bca44eac
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-24 16:31:05 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,11 +11,11 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *new;
-new = malloc(sizeof(listin_t);
+new = malloc(sizeof(listin_t));
 if (new == NULL)
 return (NULL);
 new->n = n;
-new->next = head;
+new->next = *head;
 *head = new;
 return (new);
 }
