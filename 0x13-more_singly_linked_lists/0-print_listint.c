354f98157c8d04027d03b7a028c3a2ffc2e08e49
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-24 14:56:36 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
   nnodes = 0;
   while (h != NULL)
     {
-      printf("%d", h->n);
+      printf("%d\n", h->n);
       h = h->next;
       nnodes++;
     }
