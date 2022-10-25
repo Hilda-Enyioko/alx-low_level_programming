1b2f205cb4b6ce1b890ec20104b9a917bb11d0b2
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-25 14:06:56 +0100

    Create 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to the head node of the linked list
+ * @idx: index of the list where the new node should be added
+ * Index starts at 0
+ * Return: the address of the new node
+ * or NULL if it failed
+ * (or if it is not possible to add the new node at index idx)
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *new;
+listint_t *image; /*image of the head node*/
+unsigned int index;
+new = malloc(sizeof(listint_t);
+if (new == NULL)
+return (NULL);
+new->n = n;
+index = 0;
+if (index == 0) /*if index in in the head position*/
+{
+new->next = *head;
+*head = new;
+}
+while (index < (idx - 1))
+{
+if (image == NULL || image->next == NULL)
+return (NULL);
+image = image->next;
+index++;
+}
+new->next = image->next;
+image = new;
+return (new);
+}
