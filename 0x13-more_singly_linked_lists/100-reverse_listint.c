8bf038c1ff4515cc9a3b5ca57bd3f3cf994919a5
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-25 19:43:26 +0100

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,6 +4,9 @@
  * reverse_listint - reverses a listint_t linked list
  * @head: pointer to the head node of the linked list
  * Return: a pointer to the first node of the reversed list
+ */
 listint_t *reverse_listint(listint_t **head)
 {
+listint_t *temp;
+listint_t *tail;
 }
