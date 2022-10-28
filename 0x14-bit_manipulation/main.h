bbea30b406121723098a430e4a2976f96f98e684
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-28 19:10:06 +0100

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,7 @@ int rev_index(int i, int j);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
