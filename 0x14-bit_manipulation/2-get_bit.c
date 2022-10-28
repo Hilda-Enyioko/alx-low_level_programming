cffe22902954589835343c9862b1fd48fe806729
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-28 19:07:54 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 int value;
-if (index >= (sizeof(unsigned long int n) * 8))
+if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
 value = (n >> index) & 1;
 return (value);
