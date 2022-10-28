f036fca9a51939edb8fae85615d6df3ac9de2fca
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-28 17:12:13 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -30,6 +30,7 @@ for (i = 0; b[i] != '\0'; i++)
 unsigned int binary_to_uint(const char *b)
 {
   int i;
+int j;
   unsigned int convert;
   if (b[i] != '0' || b[i] != '1' || b[i] == '\0')
     return (0);
@@ -38,7 +39,7 @@ unsigned int binary_to_uint(const char *b)
   {
     rev_index(i, j);
     while (j--)
-      convert += b[i] x '2^j';
+      convert += b[i] x ('2'^j);
   }
   return (0);
 }
