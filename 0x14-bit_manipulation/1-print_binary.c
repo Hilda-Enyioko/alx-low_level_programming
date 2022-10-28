6043ed822b8ec9034d581ce4b81c88ebac257c6b
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-28 17:48:42 +0100

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be printed
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+}
