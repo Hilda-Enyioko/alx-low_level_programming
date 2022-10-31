6fa07767626f543421477b694aa3fe77581af261
Hilda Enyioko <enyiokohilda@gmail.com>
2022-10-31 09:02:53 -0700

    creates file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: On success, 1
+ *         On failure, -1(if file cannot be created,
+ *                         file cannot be written,
+ *                         write fails, etc.)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+  int o, w, strlen;
+
+  strlen = 0;
+  
+  if (filename == NULL)
+    return (-1);
+
+  o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+  w = write(o, text_content, strlen);
+
+  if (o == -1 || w == -1)
+    return (-1);
+
+  close(o);
+  return (1);
+}
