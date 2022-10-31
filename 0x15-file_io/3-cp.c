d044b291ed26978fcd556b12c70901c640133049
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-31 19:03:31 +0100

    task 3 update: fix compiler errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 
-//open and read file_from
-//open and/or create file_to
-//copy the contents of the file that contains the read content of file_from
-//create a usage command
-
 char *create_buffer(char *filename);
 void close_file(int fd);
 
