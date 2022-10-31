889247b63d5d44c862f6576a12c1a650e0349ec4
Hilda Enyioko <enyiokohilda@gmail.com>
2022-10-31 10:40:42 -0700

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdlib.h>
+
+//open and read file_from
+//open and/or create file_to
+//copy the contents of the file that contains the read content of file_from
+//create a usage command
+
