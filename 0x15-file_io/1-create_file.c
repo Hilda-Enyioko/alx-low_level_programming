115e69007f4f50d15ccfb3a9713cf117558f7209
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-31 17:12:52 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 int o, w, strlen;
 
 strlen = 0;
-  
+
 if (filename == NULL)
 return (-1);
 
@@ -25,7 +25,7 @@ if (text_content != NULL)
 while (text_content[strlen])
 strlen++;
 }
-  
+
 o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 w = write(o, text_content, strlen);
 
