3415fa3f8cb9e10cf884f36e76e90b285d4246bc
Hilda Enyioko <enyiokohilda@gmail.com>
2022-10-31 09:09:09 -0700

    task 1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,12 @@ int create_file(const char *filename, char *text_content)
   if (filename == NULL)
     return (-1);
 
+  if (text_content != NULL)
+    {
+      while (text_content[strlen])
+	strlen++;
+    }
+  
   o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
   w = write(o, text_content, strlen);
 
